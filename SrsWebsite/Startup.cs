1e3d8d0ad3b02a9fe6fa61eeafb9071a2356a3b3
Niels Lauritsen <nmlthedane@gmail.com>
2018-08-21 12:37:18 +0200

    Auto stash before merge of "develop" and "origin/develop"

--- SrsWebsite/Startup.cs
+++ SrsWebsite/Startup.cs
@@ -7,7 +7,8 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.EntityFrameworkCore;
-using SrsWebsite.Models;
+ using Microsoft.AspNetCore.SignalR;
+ using SrsWebsite.Models;
 
 namespace SrsWebsite
 {
@@ -28,6 +29,8 @@ namespace SrsWebsite
             //var connection = @"Server=(localdb)\mssqllocaldb;Database=EFGetStarted.AspNetCore.NewDb;Trusted_Connection=True;ConnectRetryCount=0";
             var connection = @"Server=tcp:shootingregistrationsystem.database.windows.net,1433;Initial Catalog=SrsDB;Persist Security Info=False;User ID=srsAdmin;Password=Srs12341234;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30";
             services.AddDbContext<SrsContext>(options => options.UseSqlServer(connection));
+
+            services.AddSignalR();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
